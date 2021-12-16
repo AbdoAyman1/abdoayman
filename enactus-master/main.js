fe6b3eca3e3fb99d95ded00941e9f9688b7528bf
unknown <abdoaymansoli@gmail.com>
2021-12-17 01:14:04 +0200

    enactus

---
+++ enactus-master/main.js
@@ -0,0 +1,14 @@
+function originalmode(){
+
+
+    document.body.style.backgroundColor="white";
+    document.body.style.transition="all 1s ease";
+
+}
+
+function dark(){
+
+document.body.style.backgroundColor="black";
+ document.body.style.transition="all 1s ease";
+
+}
